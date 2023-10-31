4292707ae09d2d14d1ceb2166cd045cdb06f2a24
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-10-31 16:35:36 -0500

    added color variables

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import './App.css';
 import "./fonts.css"
-import ArrowLeft from './icons/Arrow-left';
-import ArrowRight from './icons/Arrow-right';
-import Artist from './icons/Artist';
-import Calendar from './icons/Calendar';
-import Facebook from './icons/Facebook';
-import Home from './icons/Home';
-import Instagram from './icons/Instagram';
-import Music from './icons/Music';
-import Play from './icons/Play';
-import Record from './icons/Record';
-import Twitter from './icons/Twitter';
-import Youtube from './icons/Youtube';
 
 function App() {
   return (
