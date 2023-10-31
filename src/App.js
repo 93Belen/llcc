89ca72bf8a370587084c55eea7bcbb434e5c710a
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-10-31 16:28:07 -0500

    added icons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import "./fonts.css"
+import ArrowLeft from './icons/Arrow-left';
+import ArrowRight from './icons/Arrow-right';
+import Artist from './icons/Artist';
+import Calendar from './icons/Calendar';
+import Facebook from './icons/Facebook';
+import Home from './icons/Home';
+import Instagram from './icons/Instagram';
+import Music from './icons/Music';
+import Play from './icons/Play';
+import Record from './icons/Record';
+import Twitter from './icons/Twitter';
+import Youtube from './icons/Youtube';
 
 function App() {
   return (
     <main className='main'>
-        <p className='large-body'>Header</p>
-        <p className='regular-body'>Header</p>
-        <p className='small-body'>Header</p>
-        <p className='nav-text'>Header</p>
-        <p className='video-button'>Header</p>
-        <p className='all-caps'>Header</p>
-        <p className='button-text'>Header</p>
-        <p className='field-label'>Header</p>
-        <p className='field-text'>Header</p>
+      
     </main>
   );
 }
