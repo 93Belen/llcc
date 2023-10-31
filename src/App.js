49874150ee468415ba5b22b31edc8b4063418531
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-10-31 15:35:42 -0500

    added font styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import "./fonts.css"
 
 function App() {
   return (
     <main className='main'>
-
+        <p className='large-body'>Header</p>
+        <p className='regular-body'>Header</p>
+        <p className='small-body'>Header</p>
+        <p className='nav-text'>Header</p>
+        <p className='video-button'>Header</p>
+        <p className='all-caps'>Header</p>
+        <p className='button-text'>Header</p>
+        <p className='field-label'>Header</p>
+        <p className='field-text'>Header</p>
     </main>
   );
 }
 
 export default App;
+
