be594831f0142b033288566cea471ad7eec9cbeb
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-11-03 16:19:22 -0500

    map working, but need a pay account

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,23 @@
-export default function Map() {
+import { useState } from "react"
+import { 
+APIProvider,
+Map,
+AdvancedMarker,
+Pin,
+InfoWindow
+} from "@vis.gl/react-google-maps"
+
+
+export default function MapElement() {
+    const position = { lat: 38.987122, lng: -94.668488}
+
+
+
     return (
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3101.2295208297833!2d-94.67097222319373!3d38.98725777170502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c0ec7261b09b7d%3A0xac8000249157d1ab!2s7815%20Floyd%20St%2C%20Overland%20Park%2C%20KS%2066204!5e0!3m2!1sen!2sus!4v1699043594470!5m2!1sen!2sus" 
-        width="100%" 
-        height="544"
-        title='map'
-        allowfullscreen="" 
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <APIProvider apiKey={process.env.REACT_APP_API_KEY}>
+            <div style={{width: '100%', height: '544px'}}>
+                <Map mapId={process.env.REACT_APP_MAP_ID} zoom={9} center={position}></Map>
+            </div>
+        </APIProvider>
     )
 }
