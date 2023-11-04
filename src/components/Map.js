723ba00d7fbdbc92d61b1c810ac5656dea5116fe
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-11-04 16:40:11 -0500

    map working and looking good

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,14 +7,13 @@ Pin
 
 
 export default function MapElement() {
-    const position = { lat: 38.987122, lng: -94.668488}
-
+    const position = { lat: 38.98731484810369, lng: -94.66842496808997}
 
 
     return (
         <APIProvider apiKey={process.env.REACT_APP_API_KEY}>
             <div style={{width: '100%', height: '544px'}}>
-                <Map mapId={process.env.REACT_APP_MAP_ID} zoom={20} center={position}>
+                <Map mapId={process.env.REACT_APP_MAP_ID} zoom={15} center={position}>
                     <AdvancedMarker position={position}>
                         <Pin background={"#42A418"} borderColor={"#42A418"} glyphColor={"#D9F0D0"} />
                     </AdvancedMarker>
